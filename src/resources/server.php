7e49692d4155ec8612846cb369607a47867af332
Inko Perurena <inkoperurena@gmail.com>
2011-01-17 13:28:41 +0000

    close #414 add 1.0h - It seems there's a bug with the addDirectory() method of Zend framework when many requests have to be served at the same time. The bug is noted on their bugtrack but it's still not fixed. Therefore whenever we have a problem of this kind the solution is to add the class explicitly.

--- src/resources/server.php
+++ src/resources/server.php
@@ -4,6 +4,8 @@
 require_once 'Zend/Loader.php';
 require_once 'Zend/Amf/Server.php';
 
+define ('SERVICE_PATH', '/amfphp/services/babelia/');
+
 /** Bootstrap */
 
 // Instantiate server
@@ -12,10 +14,21 @@ $server = new Zend_Amf_Server();
 // Disabling production shows more detailed errors.
 $server->setProduction(false);
 
+
+/**
+ * The addDirectory method is pretty unrealiable (specially when several requests come at the same time) so if you have problems with certain
+ * classes use require_once 'path/to/ClassName.php' and $server->setClass('ClassName'). This way the service class is explicitly instantiated
+ * and no other problems should arise.
+ */
+
+require_once dirname(__FILE__) . SERVICE_PATH . 'EvaluationDAO.php';
+$server->setClass('EvaluationDAO');
+
+
 //Add directories reachable for Zend AMF
-$server->addDirectory(dirname(__FILE__) .'/amfphp/services/babelia');
-$server->addDirectory(dirname(__FILE__) .'/amfphp/services/babelia/utils');
-$server->addDirectory(dirname(__FILE__) .'/amfphp/services/babelia/vo');
+$server->addDirectory(dirname(__FILE__) . SERVICE_PATH);
+$server->addDirectory(dirname(__FILE__) . SERVICE_PATH . 'utils');
+$server->addDirectory(dirname(__FILE__) . SERVICE_PATH . 'vo');
 
 // Handle request
 $response = $server->handle();
