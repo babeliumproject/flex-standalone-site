587096b355d75a2991256707088512ca9bd6597c
Inko Perurena <inkoperurena@gmail.com>
2011-02-24 11:39:47 +0100

    Changed the webhook to use googlecode's authentication method HMAC-MD5

--- src/resources/services/utils/webhook.php
+++ src/resources/services/utils/webhook.php
@@ -3,23 +3,29 @@
 require_once 'Config.php';
 require_once 'Datasource.php';
 
-
+$settings = new Config();
 $json_input = file_get_contents('php://input');
 $commit_info = json_decode($json_input, true);
 $appRevision = $commit_info['revision'];
 
-if(!empty($appRevision) && is_numeric($appRevision)){
-	$sql = "UPDATE preferences SET prefValue= '%s' WHERE (prefName='appRevision')";
-	error_log("[Webhook] Commited revision = ".$appRevision."\n", 3, "/tmp/error.log");
-	_databaseUpdate($sql, $appRevision);
-} else {
-	error_log("[Webhook] No input received\n", 3, "/tmp/error.log");
-}
+$projectSecretKey = $settings->project_secret_key;
+$digest = hash_hmac("md5",$json_input,$projectSecretKey);
 
-function _databaseUpdate() {
-	$settings = new Config ( );
-	$conn = new Datasource ( $settings->host, $settings->db_name, $settings->db_username, $settings->db_password );
-    $conn->_execute ( func_get_args() );
+$headers = apache_request_headers();
+
+if ($digest == $header["Google-Code-Project-Hosting-Hook-Hmac"]){
+	if(!empty($appRevision) && is_numeric($appRevision)){
+		$sql = "UPDATE preferences SET prefValue= '%s' WHERE (prefName='appRevision')";
+
+		$conn = new Datasource ( $settings->host, $settings->db_name, $settings->db_username, $settings->db_password );
+		$conn->_execute ( $sql, $appRevision );
+		error_log("[Webhook] Commited revision = ".$appRevision."\n", 3, $settings->webRootPath.'/logs/error.log');
+
+	} else {
+		error_log("[Webhook] No input received\n", 3, $settings->webRootPath.'/logs/error.log');
+	}
+} else {
+	error_log("[Webhook] Authentication failed.\n", 3, $settings->webRootPath.'/logs/error.log');
 }
 
 
