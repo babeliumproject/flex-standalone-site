efd85051bdee5bfd54cd878450ebfdaa3ceb21d8
Inko Perurena <inkoperurena@gmail.com>
2012-05-23 20:59:59 +0200

    CERFL descriptor tables in the database + 3 locales data

---
+++ src/resources/sql/incremental/rev0aae75aff1b7_exdescriptorschema.sql
@@ -0,0 +1,22 @@
+CREATE  TABLE `exercise_descriptor` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `level` ENUM('A1','A2','B1','B2','C1','C2') NOT NULL ,
+  `type` ENUM('L','R','SI','SP','S','LQ','W') NOT NULL ,
+  `number` INT UNSIGNED NOT NULL ,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY(`level`,`type`,`number`)
+) ENGINE = InnoDB;
+
+
+CREATE  TABLE `exercise_descriptor_i18n` (
+  `fk_exercise_descriptor_id` INT UNSIGNED NOT NULL ,
+  `locale` VARCHAR(8) NOT NULL ,
+  `name` TEXT NOT NULL ,
+  INDEX `fk_exercise_descriptor_i18n_1` (`fk_exercise_descriptor_id` ASC) ,
+  UNIQUE KEY (`fk_exercise_descriptor_id`,`locale`),
+  CONSTRAINT `fk_exercise_descriptor_i18n_1`
+    FOREIGN KEY (`fk_exercise_descriptor_id` )
+    REFERENCES `babeliumproject`.`exercise_descriptor` (`id` )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
