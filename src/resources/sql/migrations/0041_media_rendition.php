f5d68e7e88872b59bef35f4c3b54753ec1910938
Immo Goltz <immo.goltz@gmail.com>
2018-12-21 16:13:12 +0100

    fix sql error Field 'timemodified' doesn't have a default value thrown in services/Create.php

--- src/resources/sql/migrations/0041_media_rendition.php
+++ src/resources/sql/migrations/0041_media_rendition.php
@@ -10,7 +10,7 @@ function forwards(){
 				`contenthash` varchar(40) NOT NULL,
 				`status` tinyint(10) NOT NULL DEFAULT '0' COMMENT '0: raw 1: encoding, 2: ready, 3: duplicate, 4: error',
 				`timecreated` int(11) NOT NULL,
-				`timemodified` int(11) NOT NULL,
+				`timemodified` int(11) NOT NULL DEFAULT '0',
 				`filesize` int(11) NOT NULL DEFAULT '0',
 				`metadata` text,
 				`dimension` int(10) DEFAULT NULL,
