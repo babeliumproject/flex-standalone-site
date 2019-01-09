45c29491b038de4d0be576a05a3fa4ea207654a8
Immo Goltz <immo.goltz@gmail.com>
2019-01-09 14:17:45 +0100

    fix for PHP Notice:  A non well formed numeric value encountered

--- src/resources/upload.php
+++ src/resources/upload.php
@@ -200,6 +200,7 @@ function build_result_xml($header, $status, $response) {
 function return_bytes($val) {
     $val = trim($val);
     $last = strtolower($val[strlen($val)-1]);
+    $val = (int)$val;
     switch($last) {
     	// The 'G' modifier is available since PHP 5.1.0
        	case 'g':
